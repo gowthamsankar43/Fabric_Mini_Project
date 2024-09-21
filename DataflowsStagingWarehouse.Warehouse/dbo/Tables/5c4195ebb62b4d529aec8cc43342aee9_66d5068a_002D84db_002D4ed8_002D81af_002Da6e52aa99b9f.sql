f6cf855bb8b951f6be2d4d075a17d66c020eae8f
gowthamsankar43 <132737337+gowthamsankar43@users.noreply.github.com>
2024-09-21 15:49:55 +0530

    Committing 4 items from workspace ba5c5306-83f4-4f0e-b89d-6619e74d053f

---
+++ DataflowsStagingWarehouse.Warehouse/dbo/Tables/5c4195ebb62b4d529aec8cc43342aee9_66d5068a_002D84db_002D4ed8_002D81af_002Da6e52aa99b9f.sql
@@ -0,0 +1,15 @@
+CREATE TABLE [dbo].[5c4195ebb62b4d529aec8cc43342aee9_66d5068a_002D84db_002D4ed8_002D81af_002Da6e52aa99b9f] (
+
+	[Column1] varchar(8000) NULL, 
+	[Column2] date NULL, 
+	[Column3] date NULL, 
+	[Column4] varchar(8000) NULL, 
+	[Column5] varchar(8000) NULL, 
+	[Column6] varchar(8000) NULL, 
+	[Column7] varchar(8000) NULL, 
+	[Column8] varchar(8000) NULL, 
+	[Column9] bigint NULL, 
+	[Column10] float NULL, 
+	[Column11] float NULL
+);
+
---
+++ DataflowsStagingWarehouse.Warehouse/dbo/Tables/5c4195ebb62b4d529aec8cc43342aee9_72912a0a_002D7cc6_002D4656_002Da9c9_002Ddbfdb7596b2c.sql
@@ -0,0 +1,14 @@
+CREATE TABLE [dbo].[5c4195ebb62b4d529aec8cc43342aee9_72912a0a_002D7cc6_002D4656_002Da9c9_002Ddbfdb7596b2c] (
+
+	[Column1] varchar(8000) NULL, 
+	[Column2] date NULL, 
+	[Column3] date NULL, 
+	[Column4] bigint NULL, 
+	[Column5] float NULL, 
+	[Column6] varchar(8000) NULL, 
+	[Column7] varchar(8000) NULL, 
+	[Column8] bigint NULL, 
+	[Column9] float NULL, 
+	[Column10] varchar(8000) NULL
+);
+
