f69e3aa142e97e565e77bfce0db3ba365d81ea6c
gowthamsankar43 <132737337+gowthamsankar43@users.noreply.github.com>
2024-09-21 11:09:00 +0530

    Committing 2 items from workspace 7ce72a39-8ce5-4467-a316-759089c0b44a

---
+++ Fabric_Mini_Project_Prod/Project_Warehouse.Warehouse/staging/Tables/taxi_zone_lookup.sql
@@ -0,0 +1,8 @@
+CREATE TABLE [staging].[taxi_zone_lookup] (
+
+	[LocationID] varchar(8000) NULL, 
+	[Borough] varchar(8000) NULL, 
+	[Zone] varchar(8000) NULL, 
+	[service_zone] varchar(8000) NULL
+);
+
